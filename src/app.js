c708e262f3f583946614dda7b44fd220b67426dd
Maximiano Leite <macio.pb.cg@gmail.com>
2022-01-17 05:50:43 -0300

    começando a documentação

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
 const { routes } = require('./routes');
 const { database: { connection} } = require('./helps');
+const swaggerDocs = require('./swagger.json');
+
 
 const app = express();
 connection();
@@ -9,5 +12,6 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 app.use(routes);
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 module.exports = { app };
